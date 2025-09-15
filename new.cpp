e57cb581fbe6ad7862a273b2db300dc6d0f6b4d3
Aditya S Malagi <adityamalagi64@gmail.com>
2025-09-15 10:27:38 +0530

    codeforce

---
+++ new.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int sum = 0;
+    int count = 0;
+    int arr[3];
+
+    for(int i =0;i<=n;i++){
+        sum = 0;
+        for(int j =0;j <= 3;j++){
+            cin >> arr[j];
+            sum += arr[j];
+        }
+        if(sum >= 2){
+            count++;
+        }
+    }
+    
+    cout <<count << endl;
+    return 0;
+}
