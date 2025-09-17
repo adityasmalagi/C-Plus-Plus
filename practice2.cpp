e627eb1c6e71169a2779b80ef74073bcb0a1d432
Aditya S Malagi <adityamalagi64@gmail.com>
2025-09-17 19:35:45 +0530

    file

--- practice2.cpp
+++ practice2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using  namespace std;
 
 class Animal{
@@ -20,5 +21,18 @@ int main(){
     Dog d;
     a = &d;
     a->sound();
+
+cout << endl;
+    ofstream MyFile("filename.txt");
+    MyFile << "Files can be tricky, but it is fun enough!";
+    MyFile.close();
+
+    string myText;
+    ifstream MyReadFile("filename.txt");
+
+    while(getline (MyReadFile,myText)){
+        cout << myText;
+    }
+    MyReadFile.close();
     return 0;
 }
