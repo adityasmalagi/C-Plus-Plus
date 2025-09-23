331cea35b47070c650dac2bf6349594b1c5a401e
Aditya S Malagi <adityamalagi64@gmail.com>
2025-09-23 09:36:39 +0530

    jupiter

--- codeforce_4th.cpp
+++ codeforce_4th.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-    int l,b;
-    cin >> l >> b;
-    int year = 0;
+    long long l,b;
+    std::cin >> l >> b;
+    long long year = 0;
    while(l <= b){
     l *=3 ;
     b *=2;
     year++;
    }
-   cout << year << endl;
+   std::cout << year << std::endl;
     return 0;
 }
